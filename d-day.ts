0a3659acf440a7b271d62c17fb46841ab1be06ad
songhwee1 <songhwee1@naver.com>
2021-07-02 10:44:02 +0900

    change algorithm -> utc time to local time

--- d-day.ts
+++ d-day.ts
@@ -1,7 +1,23 @@
 import { Application, Router } from "https://deno.land/x/oak@v7.7.0/mod.ts";
-import { dayOfYear } from 'https://deno.land/std/datetime/mod.ts'
 
 console.log(String(new Date()))
+
+const SECOND = 1e3;
+const MINUTE = SECOND * 60;
+const HOUR = MINUTE * 60;
+const DAY = HOUR * 24;
+
+function dayOfYear(date: Date): number {
+  
+    const yearStart = new Date(date);
+  
+    yearStart.setFullYear(date.getFullYear(), 0, 0);
+    const diff = date.getTime() -
+      yearStart.getTime() +
+      (yearStart.getTimezoneOffset() - date.getTimezoneOffset()) * 60 * 1000;
+  
+    return Math.floor(diff / DAY);
+  }
 
 const app = new Application();
 
